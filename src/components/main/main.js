b9bfb3b94638b30bfecba7b2817fb03a0ab8f4f9
Peter Orum <peter@peterorum.com>
2019-03-29 14:58:34 +1100

    added fetch

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,7 +5,27 @@ import { Box, Button, Image } from 'grommet';
 export const Main = () => {
   const [src, setSrc] = useState();
 
-  useEffect(() => setSrc('https://placekitten.com/800/600'), []);
+  // on mount
+  useEffect(() => {
+    getCats();
+  }, []);
+
+  const getCats = () => {
+    // eslint-disable-next-line compat/compat
+    fetch('https://api.thecatapi.com/v1/images/search', {
+      method: 'get',
+      headers: {
+        'x-api-key': '63937a0d-ea01-4880-84cf-b541906d60dc',
+      },
+    })
+      .then(response => response.json())
+      .then(json => {
+        setSrc(json[0].url);
+      })
+      .catch(error => {
+        console.error('fetch error:', error);
+      });
+  };
 
   return (
     <Box
@@ -16,10 +36,17 @@ export const Main = () => {
       direction="column"
     >
       <Box>
-        <Button label="Another" onClick={() => {}} />
+        <Button
+          label="Another"
+          onClick={() => {
+            getCats();
+          }}
+        />
       </Box>
 
-      <Box pad="medium">{src && <Image src={src} />}</Box>
+      <Box pad="medium">
+        {src && <Image src={src} fit="cover" style={{ maxHeight: '70vh' }} />}
+      </Box>
     </Box>
   );
 };
